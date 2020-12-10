b5c072d26a40fb4ee6d65892e11e060f2e454595
Josie <josie.field@sky.uk>
2020-12-10 17:08:10 +0000

    port ahoy

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const serve = require("koa-static");
 const Koa = require("koa");
 const app = new Koa();
-
+const port = process.env.PORT || 3000;
 // $ GET /package.json
 app.use(serve("."));
 
-app.listen(3000);
+app.listen(port);
 
-console.log("listening on port 3000");
+console.log("listening on port " + port);
